28032417b9abf597207cc3df6174edeff0b97bf8
Space Cadet 💻 <154667827+backendfrontflip@users.noreply.github.com>
2025-02-12 04:47:18 -0600

    Update tailwind.config.js

--- space-tourism-app/tailwind.config.js
+++ space-tourism-app/tailwind.config.js
@@ -3,7 +3,7 @@
 module.exports = {
     content: [
       "./index.html",
-      "./src/**/*.{js,ts,jsx,tsx}", // adjust the paths to fit your project structure
+      "./src/**/*.{js,ts,jsx,tsx}",
     ],
     theme: {
       extend: {},
